a3c71499722b0243965b793c8d9e6fd770aaf614
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2022-09-21 02:06:53 +0900

    Update GraphicsCommand.h

--- SoftwareRenderer/src/Renderer/GraphicsCommand.h
+++ SoftwareRenderer/src/Renderer/GraphicsCommand.h
@@ -5,6 +5,11 @@
 class GraphicsCommand {
 public:
 	void SetGraphicsCommandQueue(class GraphicsCommandQueue* queue);
+	void SetPSO(class GraphicsPipelineStateObject* pso);
+	void SetRenderTarget(class BufferBase* buffer);
+
+	void DrawVertex();
+	void DrawIndexed();
 
 private:
 	class GraphicsCommandQueue* commandQueue;
