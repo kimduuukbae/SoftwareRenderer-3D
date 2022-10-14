7d59388b9ab34a6547b9366432538ce21a254d28
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2022-10-14 22:52:05 +0900

    Update GraphicsPipeline.cpp

--- SoftwareRenderer/src/Renderer/GraphicsPipeline.cpp
+++ SoftwareRenderer/src/Renderer/GraphicsPipeline.cpp
@@ -6,17 +6,21 @@
 #include "GraphicsPixelShader.h"
 
 void GraphicsPipeline::SetGraphicsRenderOption(GraphicsRenderOption* option) {
-
+	__option = option;
 }
+
 void GraphicsPipeline::SetGraphicsInputAssembler(GraphicsInputAssembler* inputAssembler) {
-
+	__inputAssembler = inputAssembler;
 }
+
 void GraphicsPipeline::SetGraphicsVertexShader(GraphicsVertexShader* vertexShader) {
-
+	__vertexShader = vertexShader;
 }
+
 void GraphicsPipeline::SetGraphicsRasterizer(GraphicsRasterizer* rasterizer) {
-
+	__rasterizer = rasterizer;
 }
-void GraphicsPipeline::SetGraphicsPixelShader(GraphicsPixelShader* pixelShader) {
 
+void GraphicsPipeline::SetGraphicsPixelShader(GraphicsPixelShader* pixelShader) {
+	__pixelShader = pixelShader;
 }
